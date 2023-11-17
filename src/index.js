f62e937e8ce7ebff4fef00277d51a4b9d22e5922
saritamanu <sara.m.dias22@gmail.com>
2023-11-17 06:34:00 +0000

    Add footer

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <footer>Open Source Code</footer>
+    <br />
+    <footer>
+      <a href="https://github.com/saritamanu/weather-react">
+        Open Source Code{" "}
+      </a>
+      by <a href="https://github.com/saritamanu">Sara Dias</a>
+    </footer>
   </React.StrictMode>
 );
 
